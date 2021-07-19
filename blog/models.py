631c5e79691046fb447dcba4c7fdf613bde5d8c8
louixlouis <shinyk92@gmail.com>
2021-07-19 14:56:04 +0900

    포스트 리스트 카테고리.

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,12 @@ class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
+    def __str__(self):
+        return self.name
+        
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -24,7 +27,7 @@ class Post(models.Model):
     #author = models.ForeignKey(User, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]  {self.title} :: {self.author}'
